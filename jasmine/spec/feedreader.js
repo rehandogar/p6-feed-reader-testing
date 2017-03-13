ccb7e5c2174bd26ad38e24fd5da22f63a7649316
REHAN\Rehan <rehanumardogar@gmail.com>
2017-03-14 01:52:28 +0500

    test: each feed has a url and name defined
    testing that each feed object must has url and name property defined. And that the
    url and name property is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,16 +27,28 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('each feed has a url defined', function(){
+            allFeeds.forEach(function(feed){
+              expect(feed.url).toBeDefined();
+              expect(feed.url.length).not.toBe(0);
+            });
+         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('each feed has a name defined', function(){
+           allFeeds.forEach(function(feed){
+             expect(feed.name).toBeDefined();
+             expect(feed.name.length).not.toBe(0);
+           });
+         });
     });
 
 
