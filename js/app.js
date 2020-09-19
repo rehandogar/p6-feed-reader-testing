b5571a1823b0dd2a6329d2ba656c14f1f1acda39
rehanumar <rehanumardogar@gmail.com>
2020-09-19 11:54:16 +0500

    bugfix: updating google jsapi changed interface in our codebase

--- js/app.js
+++ js/app.js
@@ -86,8 +86,8 @@ function init() {
 /* Google API: Loads the Feed Reader API and defines what function
  * to call when the Feed Reader API is done loading.
  */
-google.load('feeds', '1');
-google.setOnLoadCallback(init);
+google.charts.load('feeds', '1');
+google.charts.setOnLoadCallback(init);
 
 /* All of this functionality is heavily reliant upon the DOM, so we
  * place our code in the $() function to ensure it doesn't execute
@@ -128,7 +128,8 @@ $(function() {
     /* When the menu icon is clicked on, we need to toggle a class
      * on the body to perform the hiding/showing of our menu.
      */
-    menuIcon.on('click', function() {
+    menuIcon.on('click', function(e) {
+        e.preventDefault();
         $('body').toggleClass('menu-hidden');
     });
-}());
+});
